5ba4b2b4dea1a38e956611f10d78d19561b716f9
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2018-12-14 18:29:57 +0800

    Update getFileResultB.py
    1. 更改設定寫法，為了之後增加比較案例方便。

--- getFileResultB.py
+++ getFileResultB.py
@@ -7,28 +7,28 @@ for m in range(1, 2):
     elif m == 3:
         model_name = "mric_pps"
     for pps in range(1, 4):
-        for setting in range(1, 5):
-            data_name, product_name = "", ""
-            if setting == 1:
-                data_name = "email"
-                product_name = "item_r1p3n1"
-            elif setting == 2:
-                data_name = "email"
-                product_name = "item_r1p3n2"
-            elif setting == 3:
-                data_name = "email"
-                product_name = "item_r1p3n1_a"
-            elif setting == 4:
-                data_name = "email"
-                product_name = "item_r1p3n2_a"
-            num_ratio, num_price = int(list(product_name)[list(product_name).index('r') + 1]), int(list(product_name)[list(product_name).index('p') + 1])
-            num_product = num_ratio * num_price
-            ratio_profit, ratio_budget = [[] for _ in range(num_product)], [[] for _ in range(num_product)]
-            number_seed, number_customer = [[] for _ in range(num_product)], [[] for _ in range(num_product)]
-            for total_budget in range(1, 10):
-                try:
+        for setting in range(1, 3):
+            for setting2 in range(1, 3):
+                data_name, product_name = "email", ""
+                if setting == 1:
+                    if setting2 == 1:
+                        product_name = "item_r1p3n1"
+                    elif setting2 == 2:
+                        product_name = "item_r1p3n1_a"
+                elif setting == 2:
+                    if setting2 == 1:
+                        product_name = "item_r1p3n2"
+                    elif setting2 == 2:
+                        product_name = "item_r1p3n2_a"
+                num_ratio, num_price = int(list(product_name)[list(product_name).index('r') + 1]), int(list(product_name)[list(product_name).index('p') + 1])
+                num_product = num_ratio * num_price
+                ratio_profit, ratio_budget = [[] for _ in range(num_product)], [[] for _ in range(num_product)]
+                number_seed, number_customer = [[] for _ in range(num_product)], [[] for _ in range(num_product)]
+                for total_budget in range(1, 11):
+                    # try:
                     result_name = "result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "/" + \
                                   data_name + "_" + product_name + "_b" + str(total_budget) + "_i100.txt"
+                    print(result_name)
 
                     with open(result_name) as f:
                         for lnum, line in enumerate(f):
@@ -53,30 +53,30 @@ for m in range(1, 2):
                             else:
                                 break
                     f.close()
-                except FileNotFoundError:
-                    continue
+                    # except FileNotFoundError:
+                    #     continue
 
-            fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_ratio_profit.txt", 'w')
-            for line in ratio_profit:
-                for l in line:
-                    fw.write(str(l) + "\t")
-                fw.write("\n")
-            fw.close()
-            fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_ratio_budget.txt", 'w')
-            for line in ratio_budget:
-                for l in line:
-                    fw.write(str(l) + "\t")
-                fw.write("\n")
-            fw.close()
-            fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_number_seed.txt", 'w')
-            for line in number_seed:
-                for l in line:
-                    fw.write(str(l) + "\t")
-                fw.write("\n")
-            fw.close()
-            fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_number_customer.txt", 'w')
-            for line in number_customer:
-                for l in line:
-                    fw.write(str(l) + "\t")
-                fw.write("\n")
-            fw.close()
+                fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_ratio_profit.txt", 'w')
+                for line in ratio_profit:
+                    for l in line:
+                        fw.write(str(l) + "\t")
+                    fw.write("\n")
+                fw.close()
+                fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_ratio_budget.txt", 'w')
+                for line in ratio_budget:
+                    for l in line:
+                        fw.write(str(l) + "\t")
+                    fw.write("\n")
+                fw.close()
+                fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_number_seed.txt", 'w')
+                for line in number_seed:
+                    for l in line:
+                        fw.write(str(l) + "\t")
+                    fw.write("\n")
+                fw.close()
+                fw = open("result/" + model_name + str(pps) + "/" + data_name + "_" + product_name + "_number_customer.txt", 'w')
+                for line in number_customer:
+                    for l in line:
+                        fw.write(str(l) + "\t")
+                    fw.write("\n")
+                fw.close()
